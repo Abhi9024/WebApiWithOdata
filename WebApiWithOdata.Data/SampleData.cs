2d43a2e6d854e4dc6672f9c18306c54d555707e7
Cloud Coe <emidscloudcoe@gmail.com>
2019-10-15 11:03:04 +0530

    Data change

--- WebApiWithOdata.Data/SampleData.cs
+++ WebApiWithOdata.Data/SampleData.cs
@@ -9,11 +9,11 @@ namespace WebApiWithOdata.Data
         static List<Person> persons = new List<Person>()
         {
             new Person(){ Id =1, Age=40,Name="I", Nation= Country.US },
-            new Person(){ Id =2, Age=30,Name="Bangaram", Nation= Country.UK },
-            new Person(){ Id =3, Age=60,Name="Monika", Nation= Country.India },
-            new Person(){ Id =4, Age=50,Name="Love", Nation= Country.China },
-            new Person(){ Id =5, Age=20,Name="You", Nation= Country.India },
-            new Person(){ Id =6, Age=10,Name="Sweetheart", Nation= Country.Germany }
+            new Person(){ Id =2, Age=30,Name="Love", Nation= Country.UK },
+            new Person(){ Id =3, Age=60,Name="You", Nation= Country.India },
+            new Person(){ Id =4, Age=50,Name="Amma", Nation= Country.China },
+            new Person(){ Id =5, Age=20,Name="Forever", Nation= Country.India },
+            new Person(){ Id =6, Age=10,Name="Till my last breath", Nation= Country.Germany }
         };
         private List<Person> _persons;
 
